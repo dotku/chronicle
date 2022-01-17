3734c3247ec01e43faf1e29d91bec57c3b98e067
Weijing Jay Lin <Weijing.Jay.Lin@walmart.com>
2022-01-17 12:16:40 -0800

    add walmart curise

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
+const webpack = require("webpack");
 
 module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "/chronicle/",
+  entry: {
+    app: "./src/index.js",
+    // Runtime code for hot module replacement
+    hot: "webpack/hot/dev-server.js",
+    // Dev server client for web socket transport, hot and live reload logic
+    client: "webpack-dev-server/client/index.js?hot=true&live-reload=true",
   },
   devServer: {
     static: {
@@ -14,10 +16,19 @@ module.exports = {
     },
     compress: true,
     port: 9000,
+    hot: true,
+    client: false,
   },
   plugins: [
     new CopyPlugin({
       patterns: [{ from: "public_html", to: path.resolve(__dirname, "dist") }],
     }),
+    new webpack.HotModuleReplacementPlugin(),
   ],
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/chronicle/",
+    clean: true,
+  },
 };
